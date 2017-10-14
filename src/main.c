115089a8a39dd32e481e30ba26d69be50ef62fe5
Slava Kirillov <specctral@gmail.com>
2017-10-15 02:27:46 +0300

    task2.1

--- src/main.c
+++ src/main.c
@@ -4,6 +4,30 @@
 
 #include <stdio.h>
 
-void main(){
-    printf("Hello world!\n");
+double x, eps, sqrX;
+
+double myAbs(double y) {
+    if (y < 0) {
+        return -y;
+    }
+    return y;
+}
+
+double mySqrt(double x, double eps) {
+    double x0, abs;
+    double x1 = 1;
+    do{
+        x0 = x1;
+        x1 = ((double) 1 / 2) * (x0 + x / x0);
+        abs = myAbs(x1-x0);
+    }while (abs > eps);
+    return x1;
+}
+
+void main() {
+    scanf("%lf", &eps);
+    while (scanf("%lf", &x) == 1) {
+        sqrX = mySqrt(x, eps);
+        printf("%.10g\n", sqrX);
+    }
 }
