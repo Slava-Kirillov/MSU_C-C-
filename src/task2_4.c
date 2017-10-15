25debbf344b672c94d05ba701a3adf1820495c70
Slava Kirillov <specctral@gmail.com>
2017-10-15 23:07:38 +0300

    task2.4

---
+++ src/task2_4.c
@@ -0,0 +1,17 @@
+//
+// Created by slava on 15.10.17.
+//
+
+#include <stdio.h>
+
+double str2double(char *string){
+    
+}
+
+void main(){
+    char str[100];
+    while (scanf("%s", str) == 1){
+        printf("%.10g\n", str2doble(s));
+    }
+}
+
